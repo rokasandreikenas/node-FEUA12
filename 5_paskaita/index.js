3d81547121894601620c0dc064d681127877ed73
rokasandreikenas <shyptt@gmail.com>
2024-01-31 18:39:39 +0200

    Add eslint example

--- 5_paskaita/index.js
+++ 5_paskaita/index.js
@@ -1,5 +1,9 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const port = process.env.PORTT || 8080;
+const port = process.env.PORT || 8080;
+
+const array = [{ id: 1, name: 'Rokas', surname: 'Burokas' }];
+
+const test = array.map((item) => `${item.id} ${item.name} ${item.surname} ${item.surname}`);
 
 console.log(port);
